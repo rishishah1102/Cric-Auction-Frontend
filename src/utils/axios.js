2f5baadee0f9a9879efc06b2c9c6089b5b4b85b4
rishishah1102 <rishi.shah545@gmail.com>
2025-11-07 14:34:53 +0530

    change in url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://0.0.0.0:5000/api/v1",
-  // baseURL: "https://cricket-auction-backend.vercel.app/",
+  // baseURL: "https://cricket-auction-backend.vercel.app/api/v1",
 });
 
 const authAPI = axios.create({
